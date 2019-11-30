22081056f8d69aa18f63689efe9008d04bf5e7a8
Marcelo Benedito <marcelosilvabenedito@hotmail.com>
2019-11-29 23:32:55 -0300

    Implementar desafios

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:search_cep/views/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-    theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.amber),
-    darkTheme: ThemeData(
-      brightness: Brightness.dark,
-    ),
-  ));
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        brightness: brightness, 
+        primarySwatch: Colors.amber
+      ),
+      themedWidgetBuilder: (context, theme) {
+        return new MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+          theme: theme,
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+        );
+      }
+    );
+  }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:search_cep/services/via_cep_service.dart';
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,7 +10,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+
   var _searchCepController = TextEditingController();
+  var _logradouroController = TextEditingController();
+  var _complementoController = TextEditingController();
+  var _bairroController = TextEditingController();
+  var _localidadeController = TextEditingController();
+  var _ufController = TextEditingController();
+  var _unidadeController = TextEditingController();
+  var _ibgeController = TextEditingController();
+  var _giaController = TextEditingController();
+
   bool _loading = false;
   bool _enableField = true;
   String _result;
@@ -21,31 +35,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Consultar CEP'),
-      ),
+      appBar: AppBar(title: Text('Consultar CEP'), actions: <Widget>[
+        // action button
+        IconButton(
+          icon: Icon(Icons.brightness_high),
+          onPressed: () {
+            _changeBrightness();
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.share),
+          onPressed: () {
+            Share.share('CEP: ${_searchCepController.text} \n' +
+                'Logradouro: ${_logradouroController.text} \n' +
+                'Bairro: ${_bairroController.text} \n' +
+                'Localidade: ${_localidadeController.text} \n' +
+                'UF: ${_ufController.text} \n' +
+                'Complemento: ${_complementoController.text} \n' +
+                'Unidade: ${_unidadeController.text} \n' +
+                'IBGE: ${_ibgeController.text} \n' +
+                'Gia: ${_giaController.text} \n');
+          },
+        ),
+      ]),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            _buildSearchCepTextField(),
-            _buildSearchCepButton(),
-            _buildResultForm()
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  _buildSearchCepTextField(),
+                  _buildSearchCepButton(),
+                  _buildResultForm()
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 
+  void _changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+        Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark);
+  }
+
+  RegExp re = new RegExp(r'^[0-9]');
+
   Widget _buildSearchCepTextField() {
-    return TextField(
+    return TextFormField(
       autofocus: true,
       keyboardType: TextInputType.number,
       textInputAction: TextInputAction.done,
       decoration: InputDecoration(labelText: 'Cep'),
       controller: _searchCepController,
       enabled: _enableField,
+      validator: (String arg) {
+        if (arg.length != 8)
+          return 'CEP deve conter 8 dígitos numéricos!';
+        else if (!re.hasMatch(_searchCepController.text))
+          return 'CEP deve conter apenas dígitos numéricos!';
+        else
+          return null;
+      },
     );
   }
 
@@ -78,23 +137,98 @@ class _HomePageState extends State<HomePage> {
 
   Future _searchCep() async {
     _searching(true);
-
+    _formKey.currentState.validate();
     final cep = _searchCepController.text;
 
-    final resultCep = await ViaCepService.fetchCep(cep: cep);
-    print(resultCep.localidade); // Exibindo somente a localidade no terminal
+    try {
+      final resultCep = await ViaCepService.fetchCep(cep: cep);
 
-    setState(() {
-      _result = resultCep.toJson();
-    });
+      setState(() {
+        _logradouroController.text = resultCep.logradouro;
+        _complementoController.text = resultCep.complemento;
+        _bairroController.text = resultCep.bairro;
+        _localidadeController.text = resultCep.localidade;
+        _ufController.text = resultCep.uf;
+        _unidadeController.text = resultCep.unidade;
+        _ibgeController.text = resultCep.ibge;
+        _giaController.text = resultCep.gia;
+      });
 
-    _searching(false);
+      _searching(false);
+    } catch (error) {
+      Flushbar(
+        title: "Error",
+        message: error.toString(),
+        icon: Icon(
+          Icons.info_outline,
+          size: 28.0,
+          color: Colors.red[300],
+        ),
+        backgroundColor: Colors.red,
+        boxShadows: [
+          BoxShadow(
+            color: Colors.red[800],
+            offset: Offset(0.0, 2.0),
+            blurRadius: 3.0,
+          )
+        ],
+        duration: Duration(seconds: 3),
+        leftBarIndicatorColor: Colors.red[300],
+      )..show(context);
+      _searching(false);
+      _formKey.currentState.validate();
+      return null;
+    }
   }
 
   Widget _buildResultForm() {
     return Container(
       padding: EdgeInsets.only(top: 20.0),
-      child: Text(_result ?? ''),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Logradouro'),
+            controller: _logradouroController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Complemento'),
+            controller: _complementoController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Bairro'),
+            controller: _bairroController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Localidade'),
+            controller: _localidadeController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'UF'),
+            controller: _ufController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Unidade'),
+            controller: _unidadeController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'IBGE'),
+            controller: _ibgeController,
+            enabled: false,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Gia'),
+            controller: _giaController,
+            enabled: false,
+          ),
+        ],
+      ),
     );
   }
 }
